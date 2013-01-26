8ae2c6a51b7564c97094d8294e79eeb2bcbe2308
Dolphin <dolphin.orca@gmail.com>
2013-01-27 01:17:30 +0900

    fixed timezone

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Skeeter
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
   end
 end
