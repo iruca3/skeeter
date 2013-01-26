aa00cdb4035217d955b636e7a1eb28189493c448
Dolphin <dolphin.orca@gmail.com>
2013-01-26 19:48:14 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeeter::Application.config.secret_token = 'd5633cdda76e9c80409b8ea7f7795e0d49d3d5eb9524f4be04d56e107448184e60c8fd08b2626dac67d4c2b9d03169a8dee38eb284346ad092066e21ea4757fa'
